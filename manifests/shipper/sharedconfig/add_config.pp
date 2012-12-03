714ea5c6e35a95e49bea6e19472ed4cb0b246d2d
Tobias Schuhmacher <tschuhmacher@nemeses.de>
2012-12-03 13:41:58 +0100

    make define runable without shareconfig class

--- manifests/shipper/sharedconfig/add_config.pp
+++ manifests/shipper/sharedconfig/add_config.pp
@@ -1,23 +1,25 @@
 define logstash::shipper::sharedconfig::add_config($template='', $order=1) {
 
-  if (empty($template)) {
-    $content = template($name)
-  } else {
-    $content = template($template)
-  }
+  if (defined(Class['logstash::shipper::sharedconfig'])) {
 
-  if (!defined(File['/etc/logstash'])) {
-    file { '/etc/logstash':
-      ensure => directory,
-      owner  => 'root',
-      group  => 'root',
+    if (empty($template)) {
+      $content = template($name)
+    } else {
+      $content = template($template)
+    }
+
+    if (!defined(File['/etc/logstash'])) {
+      file { '/etc/logstash':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+      }
+    }
+
+    concat::fragment { "logstash_addconfig_${name}":
+      target  => '/etc/logstash/shipper.conf',
+      order   => $order,
+      content => $content
     }
   }
-
-  concat::fragment { "logstash_addconfig_${name}":
-    target  => '/etc/logstash/shipper.conf',
-    order   => $order,
-    content => $content
-  }
-
 }
